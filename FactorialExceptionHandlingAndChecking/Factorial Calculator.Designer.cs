c0d5d14919a0ab19b69b3614675a4571c911607d
Momo Johnson <mo12g13@gmail.com>
2016-12-18 17:08:42 -0600

    Added some comments

---
+++ FactorialExceptionHandlingAndChecking/Factorial Calculator.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FactorialExceptionHandlingAndChecking
+{
+    partial class Factorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFactorialCalculate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumInput
+            // 
+            this.txtNumInput.Location = new System.Drawing.Point(87, 41);
+            this.txtNumInput.Name = "txtNumInput";
+            this.txtNumInput.Size = new System.Drawing.Size(50, 20);
+            this.txtNumInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number :";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Factorial :";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(12, 129);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(97, 33);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(198, 129);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(68, 33);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblFactorialCalculate
+            // 
+            this.lblFactorialCalculate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFactorialCalculate.Location = new System.Drawing.Point(96, 84);
+            this.lblFactorialCalculate.Name = "lblFactorialCalculate";
+            this.lblFactorialCalculate.Size = new System.Drawing.Size(195, 23);
+            this.lblFactorialCalculate.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Momo Johnson Project 5-1 Enhance";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(115, 129);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 33);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Factorial
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(336, 174);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFactorialCalculate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumInput);
+            this.Name = "Factorial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFactorialCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FactorialExceptionHandlingAndChecking/Factorial Calculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+using System.Windows.Forms;
+/*
+ * Author: Momo Johnson
+ * C# Sharp Project 5.1 
+ * This program calculates the factorial of a number from 1-20
+ * 
+ * */
+namespace FactorialExceptionHandlingAndChecking
+{
+    public partial class Factorial : Form
+    {
+        public Factorial()
+        {
+            InitializeComponent();
+        }
+
+        //An event handler that calculates the factorial once it is clicked
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            long factorialCalculatedValuse = 1;//variable for the factorial calculation
+            if (isValidData())
+            {
+                int numberValue = Int32.Parse(txtNumInput.Text);
+                for (int i = 1; i <= numberValue; i++)
+                {
+                    factorialCalculatedValuse *= i;
+                }
+                //Setting the value of the calculated factorial
+                lblFactorialCalculate.Text = factorialCalculatedValuse.ToString("n0");
+            }
+        }
+        // An event handler that exit the form when clicked
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Are you sure you want to exit?", "Confirmation Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            this.Close();
+        }
+
+        //A method that check to see if the number is an integer and if it's from 1-20
+        private bool isInteger(TextBox textBox, String name)
+        {
+            try
+            {
+                //A method that checks to see if the number is between 1-20
+                if (Double.Parse(textBox.Text) > 0 && Double.Parse(textBox.Text)<=20)
+                {
+                    return true;
+                }
+                    else
+                    {
+                    //If the number isn't above twenty, display a message seeing the user should enter number between 1-20
+                        MessageBox.Show(name + " must be a number between 1-20", "Error Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox.Clear();
+                        textBox.Focus();
+                        return false;
+                    }
+
+
+
+                }catch(FormatException)
+            {
+                //If the user enter a string, show a message telling them to enter a number
+                MessageBox.Show(name + " shouln't be characters", "Error Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+                textBox.Focus();
+                return false;
+            }
+                
+        }
+
+        //A method that checks to see if the textbox is empty
+        private bool isPresent(TextBox textBox, String name)
+        {
+            
+            if(textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        
+        //A method that checks to make sure that the data enter is valid, returns trues if data is valid
+        private bool isValidData()
+        {
+            if (!isPresent(txtNumInput, "Number"))
+                return false;
+            if (!isInteger(txtNumInput, "Number"))
+                return false;
+            return true;
+           
+        }
+        //A method that clears various controls.
+        private void ClearTextBox()
+        {
+            txtNumInput.Clear();
+            lblFactorialCalculate.Text = "";
+            
+        }
+        //A event handler that clears the various controls
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearTextBox();
+        }
+    }
+}
